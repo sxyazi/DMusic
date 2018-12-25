84bbc195ac5ad2ba07daeff04bd79f6694cbec0f
sxyazi <sxyazi@gmail.com>
2018-12-25 23:46:45 +0800

    add another links

--- app/views.py
+++ app/views.py
@@ -13,12 +13,6 @@ def home():
     return render_template('index.html', recent=recent)
 
 
-@app.route('/ranks')
-def ranks():
-    music = KKBox.ranks()
-    return render_template('ranks.html', music=music)
-
-
 @app.route('/search', methods=['POST'])
 def search():
     videos = Youtube.search(request.form['kw'])
@@ -60,3 +54,10 @@ def play(video_id):
         'upload_date': Utils.get_timestamp(info['upload_date'])})
 
     return send_from_directory(directory='../caches', filename=video_id)
+
+
+@app.route('/ranks')
+@app.route('/ranks/<typ>')
+def ranks(typ='hourly'):
+    music = KKBox.ranks(typ)
+    return render_template('ranks.html', music=music)
--- modules/kkbox.py
+++ modules/kkbox.py
@@ -6,7 +6,14 @@ import requests
 class KKBox:
 
     @staticmethod
-    def ranks():
-        source = requests.get(
-            'https://kma.kkbox.com/charts/yearly/newrelease?lang=tc&terr=tw').text
-        return json.loads(re.search('var chart = (.*?);', source).group(1))
+    def ranks(typ):
+        urls = {
+            'hourly': 'https://kma.kkbox.com/charts/hourly?lang=tc&terr=tw',
+            'daily': 'https://kma.kkbox.com/charts/daily/newrelease?lang=tc&terr=tw',
+            'weekly': 'https://kma.kkbox.com/charts/weekly/newrelease?lang=tc&terr=tw',
+            'yearly': 'https://kma.kkbox.com/charts/yearly/newrelease?lang=tc&terr=tw'}
+
+        source = requests.get(urls[typ]).text
+
+        lis = json.loads(re.search('var chart = (.*?);\n', source).group(1))
+        return lis['charts']['newrelease'] if 'charts' in lis else lis
